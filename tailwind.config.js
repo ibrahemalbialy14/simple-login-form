b1c349cf037088001f2d9ea36e9285db7a596fcc
ibrahem albialy <148761529+ibrahemalbialy14@users.noreply.github.com>
2024-07-01 03:06:41 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+       'light-blue': 'rgba(173, 216, 230, 1)',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+tailwind.config.js
